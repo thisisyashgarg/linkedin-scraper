0ab443f7feebef595a3f2c3be8afae7933ed8f83
Yash Garg <thisisyashgarg@gmail.com>
2023-05-18 23:12:11 +0530

    created a template chrome extension

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>nsadklckjalscd</h1>
+      <h2>kjsadbcjsabdkcjaksdj</h2>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
