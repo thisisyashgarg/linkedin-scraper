8639183f92eee109b97a18eeb833bd101fbeb9a6
Yash Garg <thisisyashgarg@gmail.com>
2023-05-20 21:28:18 +0530

    basic html page rendered

--- src/App.js
+++ src/App.js
@@ -1,18 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const App = () => {
-  return (
-    <div>
-      <h1>nsadklckjalscd</h1>
-      <h2>kjsadbcjsabdkcjaksdj</h2>
-    </div>
-  );
-};
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+console.log("hello world");
